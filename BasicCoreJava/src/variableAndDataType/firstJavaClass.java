9840b5980d4689658c07f145b01a028967e83a27
Omkar <omkarotari@outlook.com>
2019-09-29 11:14:24 +0530

    first Java program

---
+++ BasicCoreJava/src/variableAndDataType/firstJavaClass.java
@@ -0,0 +1,11 @@
+package variableAndDataType;
+
+public class firstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("");
+
+	}
+
+}
